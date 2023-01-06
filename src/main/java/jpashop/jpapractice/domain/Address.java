f0fa8311f071d27ffce2a168bf9a3c29c62ecc83
dongjun-Yi <djyi0223@gmail.com>
2023-01-07 01:03:22 +0900

    설정 정보 변경

--- src/main/java/jpashop/jpapractice/domain/Address.java
+++ src/main/java/jpashop/jpapractice/domain/Address.java
@@ -10,4 +10,13 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address() { //리플렉션 때문에 기본 생성자는 꼭 필요
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
