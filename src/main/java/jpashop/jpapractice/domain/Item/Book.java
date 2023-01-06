be48c2fde3195147a311a4c556993a8676ccb810
dongjun-Yi <djyi0223@gmail.com>
2023-01-06 23:36:22 +0900

    Book 엔티티 생성

--- src/main/java/jpashop/jpapractice/domain/Item/Book.java
+++ src/main/java/jpashop/jpapractice/domain/Item/Book.java
@@ -1,8 +1,13 @@
 package jpashop.jpapractice.domain.Item;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 
 @Entity
+@Getter
+@Setter
 public class Book extends Item{
 
     private String author;
