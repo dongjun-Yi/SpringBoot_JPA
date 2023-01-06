0adfc9837caf3b69b2279dcc306051e7932e9e5c
dongjun-Yi <djyi0223@gmail.com>
2023-01-06 23:39:29 +0900

    부모 엔티티(Item) 단일 테이블 전략 설정

--- src/main/java/jpashop/jpapractice/domain/Item/Item.java
+++ src/main/java/jpashop/jpapractice/domain/Item/Item.java
@@ -3,19 +3,18 @@ package jpashop.jpapractice.domain.Item;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="dtype")
 @Getter
 @Setter
 public abstract class Item {
 
     @Id
     @GeneratedValue
-    @Column(name="item_id")
+    @Column(name = "item_id")
     private Long id;
 
     private String name;
