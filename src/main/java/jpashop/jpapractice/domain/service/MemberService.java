c521d8f90dee1ee134f924f1554b161ea435cd19
dongjun-Yi <djyi0223@gmail.com>
2023-01-08 01:39:34 +0900

    Member service 생성

---
+++ src/main/java/jpashop/jpapractice/domain/service/MemberService.java
@@ -0,0 +1,49 @@
+package jpashop.jpapractice.domain.service;
+
+import jpashop.jpapractice.domain.Member;
+import jpashop.jpapractice.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor // final 붙은 필드만 가지고 생성자를 만들어줌ㄴ
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원가입
+     */
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    /**
+     * 회원 전체 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 회원 조회
+     */
+    public Member findOne(Long id){
+        return memberRepository.findOne(id);
+    }
+}
---
+++ src/main/java/jpashop/jpapractice/service/MemberService.java
@@ -0,0 +1,8 @@
+package jpashop.jpapractice.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+
+}
