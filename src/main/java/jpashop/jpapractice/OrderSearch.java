d6a7870e5e8c98b5780c50b3bec0e2e9f59bc0cd
dongjun-Yi <djyi0223@gmail.com>
2023-01-11 14:57:17 +0900

    주문 검색 기능 생성

---
+++ src/main/java/jpashop/jpapractice/OrderSearch.java
@@ -0,0 +1,12 @@
+package jpashop.jpapractice;
+
+import jpashop.jpapractice.domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderSearch {
+    private String memberName;
+    private OrderStatus orderStatus;
+}
