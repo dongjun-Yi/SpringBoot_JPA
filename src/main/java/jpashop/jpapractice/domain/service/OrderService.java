ffb54f8580787c1ca817bb0e20f2e2fc7200f02c
dongjun-Yi <djyi0223@gmail.com>
2023-01-11 13:55:06 +0900

    OrderSetvice order 수정

--- src/main/java/jpashop/jpapractice/domain/service/OrderService.java
+++ src/main/java/jpashop/jpapractice/domain/service/OrderService.java
@@ -34,7 +34,7 @@ public class OrderService {
         delivery.setAddress(member.getAddress());
 
         //주문상품 생성
-        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), item.getPrice());
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
 
         //주문 생성
         Order order = Order.createOrder(member, delivery, orderItem);
---
+++ src/test/java/jpashop/jpapractice/domain/service/OrderServiceTest.java
@@ -0,0 +1,7 @@
+package jpashop.jpapractice.domain.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceTest {
+
+}
