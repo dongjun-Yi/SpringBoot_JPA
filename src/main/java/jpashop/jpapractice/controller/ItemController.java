dc21bc85be3a88713a8e02af51611ef316ed7311
dongjun-Yi <djyi0223@gmail.com>
2023-01-12 22:04:41 +0900

    회원 목록 조회 & 상품 등록 기능 추가

---
+++ src/main/java/jpashop/jpapractice/controller/ItemController.java
@@ -0,0 +1,35 @@
+package jpashop.jpapractice.controller;
+
+import jpashop.jpapractice.domain.Item.Book;
+import jpashop.jpapractice.domain.service.ItemService;
+import jpashop.jpapractice.web.BookForm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createBookForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String createBook(BookForm form) {
+        Book book = new Book();
+        book.setAuthor(form.getAuthor());
+        book.setName(form.getName());
+        book.setIsbn(form.getIsbn());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setPrice(form.getPrice());
+        itemService.saveItem(book);
+        return "redirect:/";
+    }
+}
