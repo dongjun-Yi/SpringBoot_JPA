4c7b11ca3bc9c83e1d03a42adc3fbeb938ab6b57
dongjun-Yi <djyi0223@gmail.com>
2023-01-19 00:40:40 +0900

    영속성 컨텍스트 변경 감지로 상품 상태 변경으로 수정

--- src/main/java/jpashop/jpapractice/controller/ItemController.java
+++ src/main/java/jpashop/jpapractice/controller/ItemController.java
@@ -62,15 +62,7 @@ public class ItemController {
 
     @PostMapping("/items/{itemId}/edit")
     public String updateItem(@ModelAttribute("form") BookForm form, @PathVariable Long itemId) {
-        Book book = new Book();
-        book.setPrice(form.getPrice());
-        book.setId(form.getId());
-        book.setName(form.getName());
-        book.setAuthor(form.getAuthor());
-        book.setStockQuantity(form.getStockQuantity());
-        book.setIsbn(form.getIsbn());
-
-        itemService.saveItem(book);
+        itemService.updateItem(itemId, form.getName(), form.getPrice(), form.getStockQuantity());
         return "redirect:/items";
     }
 }
